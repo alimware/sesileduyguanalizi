f31a6f99ec3b6dd2f7dba6106df4a0cc4c41b8a4
alimware <79518039+alimware@users.noreply.github.com>
2024-06-07 11:46:47 +0300

    Update model.py

--- model/model.py
+++ model/model.py
@@ -13,13 +13,16 @@ def extract_features(file_path):
     return np.mean(mfccs.T, axis=0)
 
 # Veri ve etiketlerin yüklenmesi
-data_path = 'C:/Users/asus/Desktop/yapayzekaprojem/data/TurEV-DB-master/'
+data_path = r'C:\Users\asus\Desktop\projem\data\TurEV-DB-master'
 data = []
 labels = []
 
 # Veri setinin bulunduğu dizinlerde gezinip özellikleri çıkarma
 for emotion in ['angry', 'calm', 'happy', 'sad']:
     emotion_path = os.path.join(data_path, emotion)
+    if not os.path.exists(emotion_path):
+        print(f"Dizin mevcut değil: {emotion_path}")
+        continue
     for file_name in os.listdir(emotion_path):
         file_path = os.path.join(emotion_path, file_name)
         if os.path.isfile(file_path):
@@ -33,8 +36,8 @@ y = np.array(labels)
 # Verileri eğitim ve test setlerine ayırma
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# SVM Modeli ile eğitim
-model = SVC(kernel='linear')
+# SVM Modeli ile eğitim (probability=True)
+model = SVC(kernel='linear', probability=True)
 model.fit(X_train, y_train)
 
 # Modelin test edilmesi
@@ -43,4 +46,6 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Model accuracy: {accuracy}")
 
 # Modeli kaydetme
-joblib.dump(model, 'emotion_recognition_model.pkl')
+model_path = r'C:\Users\asus\Desktop\projem\model\emotion_recognition_model.pkl'
+joblib.dump(model, model_path)
+print(f"Model kaydedildi: {model_path}")
